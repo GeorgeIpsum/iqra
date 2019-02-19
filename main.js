f08b097467a4f58c3a088c292bce7d623679dce8
Ibrahim Saberi <georgeipsum@gmail.com>
2019-02-19 15:21:08 -0800

    init electron

---
+++ main.js
@@ -0,0 +1,9 @@
+const electron = require('electron');
+
+function createWindow() {
+  let win = new BrowserWindow({width: 800, height: 600});
+
+  win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
